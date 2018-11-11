1f44888703fd4c5abfdd959e4f5b6ac973dea206
Bharadwaj <bharadwaj.gsp@gmail.com>
2018-11-12 02:19:15 +0530

    committed changes

--- Canteen_System_Automation/urls.py
+++ Canteen_System_Automation/urls.py
@@ -21,23 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('manager/', include('Manager.urls')),
     path('canteen/', include('history.urls')),
-<<<<<<< HEAD
     path('', include('Homepage.urls')),
-
-
-    path('registration', include('Registration.urls')),
     #path('ec/',include('eat_at_canteen.urls'))
-
-=======
     path('HomePage/', include('Homepage.urls')),
-    path('canteen/',include('history.urls')),
     #path('HomePage/',include('User.urls')),
-<<<<<<< HEAD
     path('registration/',include('Registration.urls')),
     #path('ec/',include('eat_at_canteen.urls'))
-=======
-    path('registration',include('Registration.urls')),
-    path('ec/',include('eat_at_canteen.urls'))
->>>>>>> 7ebfe8cc2289f41ebf3612408c614a5aac040dbb
->>>>>>> d72f8044f6e315edcc853a5a2150cf860897a7f8
+    #path('ec/',include('eat_at_canteen.urls'))
 ]
--- Registration/views.py
+++ Registration/views.py
@@ -15,7 +15,7 @@ def register(request):
 
         if user_form.is_valid():
             user = user_form.save()
-            user.set_password(user.password)
+            #user.check_password(user.password)
             user.save()
 
             registered = True
