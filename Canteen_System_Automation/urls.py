d7d498be11c7fd7cb07eb1b2abd0f77a335573c6
SatyaRaj <vineelraj19@gmail.com>
2018-11-12 00:45:59 +0530

    Homepage Done

--- Canteen_System_Automation/urls.py
+++ Canteen_System_Automation/urls.py
@@ -20,12 +20,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('manager/', include('Manager.urls')),
     path('canteen/', include('history.urls')),
-    path('HomePage/', include('Homepage.urls')),
-    path('canteen/',include('history.urls')),
-<<<<<<< HEAD
-    #path('HomePage/',include('User.urls')),
-=======
-    path('registration',include('Registration.urls')),
+    path('', include('Homepage.urls')),
+
+
+    path('registration', include('Registration.urls')),
     #path('ec/',include('eat_at_canteen.urls'))
->>>>>>> a672fc9c9d194b75628a25167a71cd136c896203
+
 ]
