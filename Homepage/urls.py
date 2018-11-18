b1d4bf3f960bc5a52a5af0b03736d1e5f759e57a
Bharadwaj <bharadwaj.gsp@gmail.com>
2018-11-18 17:57:27 +0530

    changed

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -3,9 +3,6 @@ from Homepage import views
 
 app_name = "Homepage"
 urlpatterns = [
-<<<<<<< HEAD
-    path("",views.default ,name="home")
-=======
+    path("",views.default ,name="home"),
     path("", views.default, name="home")
->>>>>>> 80590861a2232e7441610c2c7f35d8820c68a312
 ]
